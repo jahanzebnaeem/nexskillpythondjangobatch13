d79119bfe69b24628d788623aa5275d58f59d0ae
Jahanzeb Naeem (Jazy) <jahanzeb.naeem@hotmail.com>
2023-08-26 13:46:45 +0500

    Lecture 15
    Working with models & templates

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    top_name = models.CharField(max_length=265, unique=True)
+
+    def __str__(self):
+        return self.top_name
+    
+class Webpage(models.Model):
+    category = models.ForeignKey(Topic, on_delete=models.CASCADE) 
+    name = models.CharField(max_length=264) 
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 def home(request):
     # return HttpResponse("<h1>Welcome to the class</h1>")
     # return render(request, "index.html")
-    context = {"myMsg":"Hello how are you"}
-    return render(request, "first_app/index.html", context)
+    # context = {"myMsg":"Hello how are you"}
+    # return render(request, "first_app/index.html", context)
+    topic_list = Topic.objects.all()
+    topic_list_dict = {'topic_dict':topic_list}
+    return render(request, "first_app/index.html", topic_list_dict)
